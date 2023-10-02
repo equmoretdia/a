386f5e46cd27b87966fe3e0905b6663197335060
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-10-03 01:30:23 +0200

    task 3 completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+var throttle = require('lodash.throttle');
 
+const feedbackForm = document.querySelector('.feedback-form');
+// const inputEmail = document.querySelector('.feedback-form input');
+// const inputMessage = document.querySelector('.feedback-form textarea');
+// const submitButton = document.querySelector('.feedback-form button');
+let savedData = null;
+
+const feedbackFormData = {
+  Email: '',
+  Message: '',
+};
+
+function setItem() {
+  feedbackFormData['Email'] = feedbackForm.elements.email.value;
+  feedbackFormData['Message'] = feedbackForm.elements.message.value;
+  console.log(feedbackFormData);
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackFormData));
+}
+
+const throttledSetItem = throttle(setItem, 1000);
+
+function getItem() {
+  savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    const loadedData = JSON.parse(savedData);
+    console.log(loadedData);
+    feedbackForm.elements.email.value = loadedData['Email'];
+    feedbackForm.elements.message.value = loadedData['Message'];
+  }
+}
+
+function removeItem(event) {
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  console.log(feedbackFormData);
+  feedbackForm.reset();
+}
+
+getItem();
+
+feedbackForm.addEventListener('input', throttledSetItem);
+feedbackForm.addEventListener('submit', removeItem);
