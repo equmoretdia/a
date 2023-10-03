b5f92a49c963d201f096007500e2ad4114b8df8c
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-10-03 10:33:06 +0200

    task 2 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const player = new Player('vimeo-player');
+
+function getItem() {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    const loadedTime = JSON.parse(savedTime);
+    player.setCurrentTime(loadedTime['seconds']);
+  }
+}
+
+getItem();
+
+const throttledTimeUpdate = throttle(currentTime => {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+}, 1000);
+
+player.on('timeupdate', throttledTimeUpdate);
